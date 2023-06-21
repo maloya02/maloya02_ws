c8b5ad4d01dd38843ae8190bc7c09a758a217517
maloya02 <maloya02@gmail.com>
2023-06-21 21:53:53 +0900

    hello world init

--- node-starter/server.js
+++ node-starter/server.js
@@ -1,1 +1,26 @@
 // entry point
+
+const express = require("express");
+
+const app = express();
+
+// configure server
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+
+// create routes
+app.get('/hello',(req,res) => {
+    res.send('Hello Aa From World')
+    console.log('Hello AA from World');
+})
+
+app.get('/hello-Aadasalla', (req,res) => {
+    res.send('Hello AA from World');
+})
+
+const PORT = 8000
+app.listen(PORT, () => {
+    console.log(`Server is currently running on port ${PORT}`);
+});
