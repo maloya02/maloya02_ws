88404da58d347e40a636c456106f588e3a85fcdc
maloya02 <maloya02@gmail.com>
2023-06-23 22:01:41 +0900

    tralal

--- jsdom/01_output/script.js
+++ jsdom/01_output/script.js
@@ -27,3 +27,91 @@ document.querySelector('#hello-heading').innerHTML = "Hello World to aa dasalla!
 // >>> how to declare a varialbe
 
 // var, let, const
+// var is global
+// let is scope
+// const can be both
+var name;
+
+var name = 'aadas'
+let my_name = 'aadas'
+const MY_NAME = 'PEDRO DELECRUZ'
+
+// naming var
+//name_of_var = [a-z A-Z 0-9 _$]; 
+// var name cant start with number * case sensitive
+var $return;
+var _$price;
+
+//-----
+
+let a = 7;
+let A = 8;
+
+console.log(a == A);
+let write = id => document.querySelector(`#${id}`);
+write('result').innerHTML = a == A;
+
+// operators
+// unary (+,-) --> one operand
+// binary (+,-,*,/,**,%) --> two operand
+// ternary (?:) --> 3 operand
+
+// PEMDAS
+
+
+// Equality
+// = , assigment operator
+var x = 1;
+// ==, value comparison
+var c = 2;
+var d = 3;
+c == d; // value comparison; loose comparison
+// ===, value & type compare; strict comparison
+var e = Number(7);
+var f = new Object(7);
+write(e === f); 
+
+// +=, -=, *=, /=, **=, %=
+x += 2;
+x = x + 2;
+
+// ++, --
+x++; x--;
+++x; --x;
+// ++ x += 1;
+// -- x -= 1;
+
+// != not equal
+// !== not equal not same type
+
+// Inequalities
+// >, <, >=, <=
+
+// ? ternary operator
+// ?:
+// (condition) ? (true value) : (false value);
+
+let bal = true ? 'true_output' : 'false_output';
+write(bal);
+
+// logical and, logical or, not
+// && - logical and
+// || - logical or
+// ! - not
+
+// bitwise operator
+// & - bitwise and
+// | - bitwise or
+
+
+// Data types 
+// 1. string - primitives = p
+// 2. number -p
+// 3. BigInb
+// 4. boolean - p
+// 5. Undefined - p
+// 6. Null
+// 7. Symbol
+// 8. Object - reference type = r
+// 9. Array - r
+// 10. Function - r
