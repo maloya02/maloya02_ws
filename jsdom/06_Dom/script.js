aa1038cb656543d24f2b175383d4d71e5828c0d5
maloya02 <maloya02@gmail.com>
2023-06-27 21:32:03 +0900

    jsdom

---
+++ jsdom/06_Dom/script.js
@@ -0,0 +1,39 @@
+// custom script
+// alert('Welcome to js')
+
+// setting the html child/content
+const hello = document.getElementById('hello')
+if (hello) {
+    hello.innerText = '<b>Hello dom - text lang</b>'
+    // hello.innerHTML = '<span style="color:red;weight:900;">Hello Dom</span>'
+    hello.innerHTML = '<span style="color:red;weight:900;">Hello Dom</span>'
+}
+
+// tag specific
+const pic = document.getElementById('hello-pic')
+if (pic) {
+    pic.src = './images/burger2.jpg'
+}
+
+const byId = id => document.getElementById(id);
+const alink = byId('hello-link');
+alink ? alink.href = 'https://youtube.com' : null;
+
+// change style of a tag
+const title = byId('hello');
+if (title) {
+    title.style.backgroundColor = 'red';
+    title.style.border = '5px solid black';
+    title.style.color = 'pink';
+    // 2nd syntax
+    title.style.cssText = 'color: violet !important; font-size:30px';
+    title.style.cssText = 'border: 5px solid pink !important';
+    // 3rd syntaxt
+    title.style.setProperty('color','blue','important');
+    title.style.setProperty('border','5px solid red','important');
+    
+}
+
+
+console.log('after getting hello');
+// console.dir();
