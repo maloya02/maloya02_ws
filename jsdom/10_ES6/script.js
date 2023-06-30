9ef464c4bd256cdf4c74f13a92f656d986cee7c4
maloya02 <maloya02@gmail.com>
2023-06-30 20:49:34 +0900

    spread op

--- jsdom/10_ES6/script.js
+++ jsdom/10_ES6/script.js
@@ -1,2 +1,56 @@
 // custom script
 // alert('Welcome to js')
+
+// ES5
+const car = {
+    status: 'success',
+    code: '200',
+    data: [
+        {
+            make: 'Toyota',
+            model: 'Camry',
+            year: 2018,
+        },
+        {
+            make: 'Honda',
+            model: 'ZX9',
+            year: 2022,
+        },
+    ],
+};
+
+var carMake = car.data[0].make;
+var carMake = car.data[1].make;
+var carModel = car.data[0].model;
+var carYear = car.data[0]['year'];
+
+// es6
+let { make, model, year } = car.data[0];
+
+
+const user = {
+    name: 'A.a Dasalla',
+    type: 'Master',
+    region: 'southeast-asia-3',
+    id: 'asdjkahsdhasd',
+    token: '56a4sd5a4sd',
+    lastLogin: '2023-06-30T193000',
+    model: 'GTX-696969',
+};
+
+let { name, token, id } = user;
+let { region: zone1, lastLogin } = user;
+let { model: macho } = user;
+
+const fruits = [{content:'apple'}, {content:'mango'}, {content:'pineapple'}];
+let [red, green, yellow] = fruits; // cant rename
+// spread operator = ...
+const colors = ['red','purple','green','blue','black'];
+const crayonBox = {...colors};
+const crayonWheel = [...colors];
+
+const logger = (...msg) => {
+    console.log(msg);
+} 
+
+console.log('after var val');
